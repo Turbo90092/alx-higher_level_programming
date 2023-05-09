784f50e59b3cf1c32fd15791a30dec7a014c10a2
Turbo90092 <cliveturbo@gmail.com>
2023-05-09 14:10:54 -0700

    Task102

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,14 +1,3 @@
 #!/usr/bin/python3
-
 def magic_calculation(a, b):
-    result = 1
-    for i in range(b):
-        try:
-            if i > a:
-                raise Exception('Too far')
-            else:
-                result += (a * i) + i
-        except:
-            result = b + a
-            break
-    return result
+return (98 + a ** b)
