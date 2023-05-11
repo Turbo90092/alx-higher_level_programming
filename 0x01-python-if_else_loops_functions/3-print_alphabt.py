68ee434cbcf6eb1103b5012c83bebea821f8634e
Turbo90092 <cliveturbo@gmail.com>
2023-05-11 04:06:01 -0700

    Task3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for char_code in range(97, 123):
-    if chr(char_code) not in 'eq':
-        print(chr(char_code), end='')
+for i in range(97, 123):
+    if chr(i) is not 'q' and chr(i) is not 'e':
+        print("{}".format(chr(i)), end="")
