e02a9b9c6a3c271aa3a16bfa42ddaef055c55b54
Turbo90092 <cliveturbo@gmail.com>
2023-05-16 01:32:06 -0700

    Task3

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-#!/usr/bin/env python3
-no_c = __import__('5-no_c').no_c
 
-print(no_c("Best School"))
-print(no_c("Chicago"))
-print(no_c("C is fun!"))
+def no_c(my_string):
+    new_str = ''
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_str += char
+    return (new_str)
