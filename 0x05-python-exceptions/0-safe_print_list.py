85ed09c3fb643b2b73bc976b5d3509f806eafd53
Turbo90092 <cliveturbo@gmail.com>
2023-05-29 07:21:29 -0700

    Task0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,12 @@
-#!/usr/bin/pytho3
+#!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    try:
-        count = 0
-        for i in range(x):
-            print(my_list[i], end='')
-            count += 1
-    except IndexError:
-        pass
-    finally:
-        print()
-        return (count)
+    sum_total = 0
+    for i in range(x):
+        try:
+            print(f"{my_list[i]}", end="")
+            sum_total += 1
+        except IndexError:
+            break
+    print()
+    return(sum_total)
